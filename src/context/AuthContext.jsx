6037e967d239cc599f8c3e03bee20d8f7946028f
noureldeinnn <n.elweteedy@gmail.com>
2026-03-28 20:33:36 +0200

    feat(Auth): Refactor authentication to use custom hook

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext(null)
 
@@ -43,3 +43,5 @@ export default function AuthProvider({ children }) {
         </AuthContext.Provider>
     )
 }
+
+export const useAuth = () => useContext(AuthContext)
