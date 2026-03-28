dab4b19396fb26f04ff50b786bba5e85ffcffe8f
noureldeinnn <n.elweteedy@gmail.com>
2026-03-28 16:44:33 +0200

    feat(Auth): implement conditional user authentication UI in Navbar and remove redundant logout controls from Auth page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 export default function Navbar() {
+    const { user, logout } = useContext(AuthContext)
+    const navigate = useNavigate()
     return <nav className="navbar">
         <div className="navbar-container">
             <Link className='navbar-brand' to="/">Marketplace</Link>
@@ -9,10 +13,12 @@ export default function Navbar() {
                 <Link to="/checkout" className='navbar-link'>Checkout</Link>
             </div>
             <div className="navbar-auth">
-                <div className='navbar-auth-links'>
-                    <Link to="/auth" className='btn btn-secondary'>Login</Link>
-                    <Link to="/auth" className='btn btn-primary'>Sign Up</Link>
-                </div>
+                {user ? (<div className="navbar-user"><span className='navbar-greeting' onClick={() => navigate('/auth')}>Hi!, {user.email}</span>
+                    <button className='btn btn-secondary' onClick={logout}>Logout</button></div>) : (
+                    <div className='navbar-auth-links'>
+                        <Link to="/auth" className='btn  btn-secondary'>Login</Link>
+                        <Link to="/auth" className='btn btn-primary'>Sign Up</Link>
+                    </div>)}
             </div>
         </div>
     </nav>
